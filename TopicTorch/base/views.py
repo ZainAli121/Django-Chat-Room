fbfa7e9661047b573215c1f20be7722abe331c53
Zain <learnerstudent001@gmail.com>
2023-10-23 21:01:11 +0500

    views and URL's

--- TopicTorch/base/views.py
+++ TopicTorch/base/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def home(request):
+    return HttpResponse("This is home page")
+
+def room(request):
+    return HttpResponse("this is room page")
