d8b81ea32d19d07ad429322448dff69ae0804ce6
Zain <learnerstudent001@gmail.com>
2023-10-24 21:08:22 +0500

    init: templates

--- TopicTorch/base/views.py
+++ TopicTorch/base/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 # Create your views here.
 
-def home(request):
-    return HttpResponse("This is home page")
+rooms = [
+    {'id': 1, 'name': 'Python'},
+    {'id': 2, 'name': 'Java'},
+    {'id': 3, 'name': 'C++'},
+]
 
-def room(request):
-    return HttpResponse("this is room page")
+def home(request):
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html', context)
+
+def room(request, pk):
+    room = None
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i
+    context = {'room': room}
+    return render(request, 'base/room.html', context)
