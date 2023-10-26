a811b6e4bf387d9c29ec3f5b9016abd6da665192
Zain <learnerstudent001@gmail.com>
2023-10-26 20:35:21 +0500

    models

--- TopicTorch/base/views.py
+++ TopicTorch/base/views.py
@@ -1,20 +1,13 @@
 from django.shortcuts import render
+from .models import *
 # Create your views here.
 
-rooms = [
-    {'id': 1, 'name': 'Python'},
-    {'id': 2, 'name': 'Java'},
-    {'id': 3, 'name': 'C++'},
-]
-
 def home(request):
+    rooms = Room.objects.all()
     context = {'rooms': rooms}
     return render(request, 'base/home.html', context)
 
 def room(request, pk):
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
+    room = Room.objects.get(id = pk)
     context = {'room': room}
     return render(request, 'base/room.html', context)
